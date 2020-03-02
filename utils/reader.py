e8d856456d23b1d4f547fa949547739737dc6cb6
Jinhong Jung <jinhongjung@snu.ac.kr>
2020-03-02 15:52:39 +0900

    add functions supporting undirected networks

--- utils/reader.py
+++ utils/reader.py
@@ -5,7 +5,7 @@ import numpy as np
 from scipy.sparse import csr_matrix, find
 
 
-def __read_directed_graph(X):
+def read_directed_graph(X, weighted):
     rows = X[:, 0]
     cols = X[:, 1]
     data = X[:, 2]
@@ -13,18 +13,50 @@ def __read_directed_graph(X):
     # assume id starts from 0
     n = int(np.amax(X[:, 0:2])) + 1
 
+    # the weights of redundant edges will be summed (by default in csr_matrix)
     A = csr_matrix((data, (rows, cols)), shape=(n, n))
+
+    if not weighted:
+        # no redundant edges are allowed for unweighted graphs
+        I, J, K = find(A)
+        A = csr_matrix((np.ones(len(K), (I, J), shape=A.shape)))
+
     return A
 
 
-def __read_undirected_graph(X):
-    _A = __read_directed_graph(X)
+def read_undirected_graph(X, weighted):
+    rows = X[:, 0]
+    cols = X[:, 1]
+    data = X[:, 2]
+
+    # assume id starts from 0
+    n = int(np.amax(X[:, 0:2])) + 1
+
+    # the weights of redundant edges will be summed (by default in csr_matrix)
+    _A = csr_matrix((data, (rows, cols)), shape=(n, n))
+
+    # this is under the assumption that src_id <= dst_id for all edges (see line 80 in this code)
     A = _A + _A.T
-    I, J, K = find(A)
 
-    A = csr_matrix((np.ones(len(K)), (I, J)), shape=A.shape)
+    if not weighted:
+        # no redundant edges are allowed for unweighted graphs
+        I, J, K = find(A)
+        A = csr_matrix((np.ones(len(K)), (I, J)), shape=A.shape)
 
     return A
+
+
+def swap_columns(X):
+    # make src_id <= dst_id
+    b_idx = X[:, 0] > X[:, 1]
+    a_idx = np.logical_not(b_idx)
+
+    B = X[b_idx, :]
+    B[:, [0, 1]] = B[:, [1, 0]] # swap columns
+
+    A = X[a_idx, :]
+
+    return np.vstack((A, B))
 
 
 def read_graph(path, graph_type):
@@ -45,11 +77,16 @@ def read_graph(path, graph_type):
     X = np.loadtxt(path, dtype=float, comments='#')
     m, n = X.shape
 
+    # make src_id <= dst_id
+    X = swap_columns(X)
+
+    weighted = True
     if n == 2:
         # the graph is unweighted
         X = np.c_[ X, np.ones(m) ]
+        weighted = False
     elif n <= 1 or n >= 4:
-        # undefined type, invoerror
+        # undefined type
         raise FormatError('Invalid input format')
 
     base = np.amin(X[:, 0:2])
@@ -60,12 +97,13 @@ def read_graph(path, graph_type):
     if min_weight < 0:
         raise ValueError('Negative edge weights')
 
+    # make node id start from 0
     X[:, 0:2] = X[:, 0:2] - base
 
     if graph_type is "directed":
-        A = __read_directed_graph(X)
+        A = read_directed_graph(X, weighted)
     elif graph_type is "undirected":
-        A = __read_undirected_graph(X)
+        A = read_undirected_graph(X, weighted)
     elif graph_type is "bipartite":
         pass
     #    A = _read_bipartite_graph(X)
